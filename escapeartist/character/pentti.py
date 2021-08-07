b353c9a90b8056a1495812fdda03de8461a4d53f
Teemu Männikkö <teemuj.mannikko@gmail.com>
2021-08-07 18:32:07 +0300

    Changed Pentti to follow col/row instead of x/y

--- escapeartist/character/pentti.py
+++ escapeartist/character/pentti.py
@@ -11,23 +11,23 @@ class Pentti:
     Pentti knows his location and can move to any available adjacent space.
     """
 
-    def __init__(self, coord_x: int, coord_y: int) -> None:
+    def __init__(self, row: int, col: int) -> None:
         self._pos = {
-            "x": coord_x, 
-            "y": coord_y,
+            "row": row,
+            "col": col,
         }
 
     def position(self) -> Tuple[int, int]:
-        return (self._pos["x"], self._pos["y"])
+        return (self._pos["row"], self._pos["col"])
+
+    def move_up(self) -> None:
+        self._pos.update({"row": self._pos["row"] - 1})
+
+    def move_right(self) -> None:
+        self._pos.update({"col": self._pos["col"] + 1})
+
+    def move_down(self) -> None:
+        self._pos.update({"row": self._pos["row"] + 1})
 
     def move_left(self) -> None:
-        self._pos.update({"x": self._pos["x"] - 1})
-    
-    def move_up(self) -> None:
-        self._pos.update({"y": self._pos["y"] + 1})
-    
-    def move_right(self) -> None:
-        self._pos.update({"x": self._pos["x"] + 1})
-    
-    def move_down(self) -> None:
-        self._pos.update({"y": self._pos["y"] - 1})
+        self._pos.update({"col": self._pos["col"] - 1})
--- tests/unit/character/test_unit_pentti.py
+++ tests/unit/character/test_unit_pentti.py
@@ -10,26 +10,26 @@ def test_init_pentti():
 
     assert pentti.position() == (0, 10)
 
-def test_move_left():
-    pentti = Pentti(10, 10)
-
-    pentti.move_left()
-    assert pentti.position() == (9, 10)
-
 def test_move_up():
     pentti = Pentti(10, 10)
 
     pentti.move_up()
-    assert pentti.position() == (10, 11)
+    assert pentti.position() == (9, 10)
 
 def test_move_right():
     pentti = Pentti(10, 10)
 
     pentti.move_right()
-    assert pentti.position() == (11, 10)
+    assert pentti.position() == (10, 11)
 
 def test_move_down():
     pentti = Pentti(10, 10)
 
     pentti.move_down()
+    assert pentti.position() == (11, 10)
+
+def test_move_left():
+    pentti = Pentti(10, 10)
+
+    pentti.move_left()
     assert pentti.position() == (10, 9)
